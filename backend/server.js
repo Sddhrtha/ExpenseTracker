c68bbbda1f4b377c78b1c80ea2c7de455cf92e57
Sddhrtha <26519057+Sddhrtha@users.noreply.github.com>
2022-11-29 16:43:51 +0530

    expenses backend added

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,27 @@
-console.log('Hello World!')
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const expensesRouter = require("../backend/routes/expenses.js");
+
+require('dotenv').config();
+
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+
+const URI = process.env.ATLAS_URI;
+mongoose.connect(URI, { useNewUrlParser: true});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
+
+app.use("/expenses", expensesRouter);
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+})
